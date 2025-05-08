0047ae16e1ed54112f5c4a9b3edbf066b85b71d9
sg <seung-g@naver.com>
2025-05-08 10:49:58 +0900

    feat: 회원가입 기능 개발

---
+++ src/main/java/org/example/studyfy/member/model/MemberRequest.java
@@ -0,0 +1,50 @@
+package org.example.studyfy.member.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import org.example.studyfy.BaseEntity;
+import org.example.studyfy.member.db.MemberEntity;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(value= PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MemberRequest  {
+
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^[MF]$", message = "성별은 'M' 또는 'F'여야 합니다.")
+    @Builder.Default
+    private String gender = "M";
+
+    // DTO → Entity 변환 메서드
+    public MemberEntity toEntity() {
+        return MemberEntity.builder()
+                .userName(this.userName)
+                .password(this.password)
+                .email(this.email)
+                .gender(this.gender)
+                .build();
+    }
+
+}
