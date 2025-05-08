efbf2cd2f889a23401320ca0adeea1e9fb4f2b5a
Shin Dong Jun <tlsehdwns147@naver.com>
2025-05-08 14:26:13 +0900

    feat: 스터디 검색기능 1차

--- src/main/java/org/example/studyfy/study/repository/StudyRepository.java
+++ src/main/java/org/example/studyfy/study/repository/StudyRepository.java
@@ -3,7 +3,12 @@ package org.example.studyfy.study.repository;
 import org.example.studyfy.study.entity.StudyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudyRepository extends JpaRepository<StudyEntity, Long> {
+
+    List<StudyEntity> findByTitleContainingOrGoalContainingOrDescriptionContaining(String title, String goal, String description);
+
 
 
 }
